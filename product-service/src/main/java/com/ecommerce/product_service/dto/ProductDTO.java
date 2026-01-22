8a2ba0768a3636071ce3b4626bf1f55131d09291
Fabiano <fabianoquirinos@outlook.com>
2026-01-22 09:59:41 -0300

    :sparkles: feat: Validations no DTO

--- product-service/src/main/java/com/ecommerce/product_service/dto/ProductDTO.java
+++ product-service/src/main/java/com/ecommerce/product_service/dto/ProductDTO.java
@@ -1,4 +1,13 @@
 package com.ecommerce.product_service.dto;
 
-public record ProductDTO(String name, String description, Double price, String category) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public record ProductDTO(@NotBlank(message = "O Nome do Produto não pode ser nulo ou vazio") String name,
+                         @NotBlank(message = "O Produto Precisa ter uma descrição") @Size(min = 10) String description,
+                         @Positive(message = "O Nome do Preco não pode ser negativo ") Double price,
+                         @NotNull (message = "O nome da Categoria não pode ser nulo") String category ) {
+
 }
