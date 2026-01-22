2249d4ad392869ba69da09b9d9466fdf6f9ca188
Fabiano <fabianoquirinos@outlook.com>
2026-01-22 10:12:44 -0300

    :sparkles: feat: Método findByID

--- product-service/src/main/java/com/ecommerce/product_service/controllers/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controllers/ProductController.java
@@ -5,6 +5,8 @@ import com.ecommerce.product_service.dto.ProductDTO;
 import com.ecommerce.product_service.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,8 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-
-    public ResponseEntity<ProductDTO> findById(){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable String id){
 
 
         return null;
--- product-service/src/main/java/com/ecommerce/product_service/services/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/services/ProductService.java
@@ -1,10 +1,27 @@
 package com.ecommerce.product_service.services;
 
 
+import com.ecommerce.product_service.dto.ProductDTO;
+import com.ecommerce.product_service.entities.Product;
+import com.ecommerce.product_service.repositories.ProductRepository;
+import com.ecommerce.product_service.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
+    @Autowired
+    private ProductRepository repository;
 
+
+    public ProductDTO findByID(String id){
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Produto não encontrado através do id" + id));
+
+
+        return new ProductDTO(product.getName(), product.getDescription(), product.getPrice(), product.getCategory());
+    }
 }
