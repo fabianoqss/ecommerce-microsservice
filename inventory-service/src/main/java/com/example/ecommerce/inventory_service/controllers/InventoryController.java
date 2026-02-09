2a07c63350ee36598eec5de2b4c567c722ce7f3a
Fabiano <fabianoquirinos@outlook.com>
2026-02-09 09:10:33 -0300

    :sparkles: feat: Initial Commit of InventoryController

--- inventory-service/src/main/java/com/example/ecommerce/inventory_service/controllers/InventoryController.java
+++ inventory-service/src/main/java/com/example/ecommerce/inventory_service/controllers/InventoryController.java
@@ -1,10 +1,25 @@
 package com.example.ecommerce.inventory_service.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ecommerce.inventory_service.dto.InventoryDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping
-@RestController("/api/")
+@RestController("/api/inventory")
 public class InventoryController {
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryDTO> isInStock(@RequestParam("skuCode")List<String> skuCode, @RequestParam Integer quantity){
+
+
+
+        return null;
+    }
+
 }
